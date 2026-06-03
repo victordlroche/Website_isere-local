9e397af340e808674bc8d1971239dd3a1ab25599
victor-domagala <victor.domagala@enpc.fr>
2026-06-03 11:46:11 +0200

    init: Pise Iserois - Astro + Decap CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // Remplace par ton username GitHub et le nom du repo
-// Ex : https://tonusername.github.io/pise-iserois
+// Ex : https://tonusername.github.io/
 export default defineConfig({
   site: 'https://victordlroche.github.io',
-  base: '/pise-iserois',
+  base: '/',
   output: 'static',
   integrations: [mdx(), sitemap()],
 });
