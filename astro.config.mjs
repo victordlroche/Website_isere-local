41c5480e999345e2820debebe3b2fb5f92938943
victor-domagala <victor.domagala@enpc.fr>
2026-06-03 12:37:10 +0200

    fix: remove sitemap to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-// Remplace par ton username GitHub et le nom du repo
-// Ex : https://tonusername.github.io/
 export default defineConfig({
   site: 'https://victordlroche.github.io/Website_isere-local',
   base: '/Website_isere-local',
   output: 'static',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
 });
